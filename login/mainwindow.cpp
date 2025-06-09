ff2c2e4cf2376578a22f0f860aeb4bbef090b0e4
RolaRefaat <rola.refaat@gmail.com>
2025-06-09 22:39:52 +0200

    GUIV1

--- login/mainwindow.cpp
+++ login/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QMessageBox>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -24,5 +25,16 @@ void MainWindow::on_LOGIN_clicked()
 void MainWindow::on_Signup_clicked()
 {
     ui->stackedWidget->setCurrentIndex(1);
+}
+
+
+void MainWindow::on_continueasaguest_clicked()
+{
+   hide();
+   // SecDialog secdialog;
+  // secdialog.setModal(true);
+  //  secdialog.exec();
+   secDialog=new SecDialog(this);
+   secDialog->show();
 }
 
