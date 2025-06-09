2c062f0f65fdf492e284353180381b4919a0169b
ameerlouly <ameer.Louly04@eng-st.cu.edu.eg>
2025-06-10 01:37:28 +0300

    Updated GitIgnore

--- login/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/ui_mainwindow.h
+++ login/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -423,7 +423,7 @@ public:
 
         retranslateUi(MainWindow);
 
-        stackedWidget->setCurrentIndex(1);
+        stackedWidget->setCurrentIndex(0);
 
 
         QMetaObject::connectSlotsByName(MainWindow);
