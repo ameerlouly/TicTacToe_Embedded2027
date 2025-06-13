2414a21097ab52ee955ff69bdb3c81036dcbfdd0
RolaRefaat <rola.refaat@gmail.com>
2025-06-13 17:55:18 +0200

    profile page

--- login/secdialog.cpp
+++ login/secdialog.cpp
@@ -15,69 +15,69 @@ SecDialog::~SecDialog()
 
 void SecDialog::on_startgame_clicked()
 {
-    ui->stackedWidget->setCurrentIndex(2);
+    ui->stackedWidget->setCurrentIndex(1);
 }
 
 
 
 void SecDialog::on_PVP_clicked()
 {
-     ui->stackedWidget->setCurrentIndex(3);
+     ui->stackedWidget->setCurrentIndex(2);
 }
 
 
 void SecDialog::on_PVE_clicked()
 {
-     ui->stackedWidget->setCurrentIndex(4);
+     ui->stackedWidget->setCurrentIndex(3);
 }
 
 
 void SecDialog::on_normal_clicked()
 {
-      ui->stackedWidget->setCurrentIndex(5);
+      ui->stackedWidget->setCurrentIndex(4);
 }
 
 
 void SecDialog::on_infinite_clicked()
 {
-      ui->stackedWidget->setCurrentIndex(5);
+      ui->stackedWidget->setCurrentIndex(4);
 }
 
 
 void SecDialog::on_easy_clicked()
 {
-      ui->stackedWidget->setCurrentIndex(5);
+      ui->stackedWidget->setCurrentIndex(4);
 }
 
 
 void SecDialog::on_medium_clicked()
 {
-    ui->stackedWidget->setCurrentIndex(5);
+    ui->stackedWidget->setCurrentIndex(4);
 
 }
 
 
 void SecDialog::on_hard_clicked()
 {
-      ui->stackedWidget->setCurrentIndex(5);
+      ui->stackedWidget->setCurrentIndex(4);
 }
 
 
 void SecDialog::on_back_clicked()
 {
-      ui->stackedWidget->setCurrentIndex(2);
+      ui->stackedWidget->setCurrentIndex(1);
 }
 
 
 void SecDialog::on_back_2_clicked()
 {
-      ui->stackedWidget->setCurrentIndex(2);
+      ui->stackedWidget->setCurrentIndex(1);
 }
 
 
 void SecDialog::on_back_3_clicked()
 {
-      ui->stackedWidget->setCurrentIndex(1);
+      ui->stackedWidget->setCurrentIndex(0);
 }
 
 void SecDialog::on_tableWidget_itemClicked(QTableWidgetItem *item)
@@ -103,5 +103,11 @@ void SecDialog::on_history_clicked()
 void SecDialog::on_settings_clicked()
 {
    ui->stackedWidget->setCurrentIndex(6);
+}
+
+
+void SecDialog::on_profile_clicked()
+{
+     ui->stackedWidget->setCurrentIndex(7);
 }
 
--- login/secdialog.h
+++ login/secdialog.h
@@ -48,6 +48,8 @@ private slots:
 
     void on_settings_clicked();
 
+    void on_profile_clicked();
+
 private:
     Ui::SecDialog *ui;
 };
